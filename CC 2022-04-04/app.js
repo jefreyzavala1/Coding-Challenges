9e1974f4c249993b19f0af48e130a13e4227a32b
Jefrey Zavala <jefreyzavala5@gmail.com>
2022-04-04 18:20:11 -0400

    Add coding challenge for 04-04-2022

--- CC 2022-04-04/app.js
+++ CC 2022-04-04/app.js
@@ -12,6 +12,7 @@
 
 // * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
 
+
 function mergeArrays(arr1, arr2) {
     let merge = arr1.filter( item => !arr2.includes(item));
      let arr3 = merge.concat(arr2);
