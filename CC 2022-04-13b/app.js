506dafaa3fe765234b334f32e1a25f1d24aaa842
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-13 16:10:44 -0400

    Add coding challenge for 04-13b-2022

---
+++ CC 2022-04-13b/app.js
@@ -0,0 +1,26 @@
+/* In this kata you are given an array to sort but you're expected to start sorting from a specific position of the array (in ascending order) and optionally you're given the number of items to sort.
+
+Examples:
+sectSort([1, 2, 5, 7, 4, 6, 3, 9, 8], 2) //=> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sectSort([9, 7, 4, 2, 5, 3, 1, 8, 6], 2, 5) //=> [9, 7, 1, 2, 3, 4, 5, 8, 6]
+Documentation:
+sectSort(array, start, length);
+array - array to sort
+start - position to begin sorting
+length - number of items to sort (optional)
+if the length argument is not passed or is zero, you sort all items to the right of the start position in the array */
+
+function sectSort(array, start, length) {
+  let newArray = [];
+  if (length == 0 || !length) {
+    newArray = array.splice(start);
+  } else {
+    newArray = array.splice(start, length);
+  }
+  newArray.sort((a, b) => a - b);
+  array.splice(start, 0, ...newArray);
+  return array;
+}
+
+console.log(sectSort([1, 2, 5, 7, 4, 6, 3, 9, 8], 2)); //[1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(sectSort([9, 7, 4, 2, 5, 3, 1, 8, 6], 2, 5)); //[9, 7, 1, 2, 3, 4, 5, 8, 6]
