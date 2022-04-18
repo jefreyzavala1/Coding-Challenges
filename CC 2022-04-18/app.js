92a0a03abd5c8229517ed78527f07f6d3b0cafce
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-18 11:17:04 -0400

    Add coding challenge for 04-18-2022

---
+++ CC 2022-04-18/app.js
@@ -0,0 +1,33 @@
+/* Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0,
+while the others must be changed to 1 (Note: 0 is considered non-negative, here). */
+
+function matrix(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (i == j) {
+        if (array[i][j] < 0) {
+          array[i][j] = 0;
+        } else if (array[i][j] >= 0) {
+          array[i][j] = 1;
+        }
+      }
+    }
+  }
+  return array;
+}
+
+console.log(
+  matrix([
+    [-1, 4, -5, -9, 3],
+    [6, -4, -7, 4, -5],
+    [3, 5, 0, -9, -1],
+    [1, 5, -7, -8, -9],
+    [-3, 2, 1, -5, 6],
+  ])
+); /* [
+    [ 0,  4, -5, -9,  3 ],
+    [ 6,  0, -7,  4, -5 ],
+    [ 3,  5,  1, -9, -1 ],
+    [ 1,  5, -7,  0, -9 ],
+    [-3,  2,  1, -5,  1 ]
+    ] */
