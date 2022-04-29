68578c2a24f1fd02566a2fa7de4d50779e908aae
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-29 16:01:27 -0400

    Add coding challenge for 04-29d-2022

---
+++ CC 2022-04-29d/app.js
@@ -0,0 +1,46 @@
+/* Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+For example, a multiplication table (string) for number == 5 looks like below:
+
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
+P. S. You can use \n in string to jump to the next line. */
+
+function multiTable(number) {
+  let multi = "";
+  for (let i = 1; i <= 9; i++) {
+    let n = i * number;
+    multi += `${i} * ${number} = ${n}\n`;
+  }
+  multi += `${10} * ${number} = ${10 * number}`;
+  return multi;
+}
+
+console.log(multiTable(5)); /* "1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50" */
+console.log(multiTable(1)); /* "1 * 1 = 1
+2 * 1 = 2
+3 * 1 = 3
+4 * 1 = 4
+5 * 1 = 5
+6 * 1 = 6
+7 * 1 = 7
+8 * 1 = 8
+9 * 1 = 9
+10 * 1 = 10" */
