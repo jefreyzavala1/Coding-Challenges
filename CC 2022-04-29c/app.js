b15708aa377339280b12537418c60c15c5cf5586
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-29 14:55:22 -0400

    Add coding challenge for 04-29c-2022

---
+++ CC 2022-04-29c/app.js
@@ -0,0 +1,29 @@
+/* In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  [] */
+
+function twoHighest(arr) {
+  let sorted = arr.sort((a, b) => b - a);
+  let array = [];
+  let j = 0;
+  for (let i = 0; i < sorted.length; i++) {
+    if (!array.includes(sorted[i])) {
+      array.push(sorted[i]);
+      j++;
+    }
+    if (j == 2) {
+      break;
+    }
+  }
+  return array;
+}
+
+console.log(twoHighest([])); //[]
+console.log(twoHighest([15])); //[15]
+console.log(twoHighest([15, 20, 20, 17])); //[20,17]
