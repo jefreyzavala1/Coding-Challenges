7cff630c75eb531cfcf310cc5e6fc1396899396b
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-05-10 16:03:41 -0400

    Add coding challenge for 05-10-2022

--- CC 2022-05-10/app.js
+++ CC 2022-05-10/app.js
@@ -9,5 +9,5 @@ function differenceInAges(ages) {
   return [min, max, max - min];
 }
 
-console.log(differenceInAges([82, 15, 6, 38, 35]));//[6,82,76]
-console.log(differenceInAges([57, 99, 14, 32]));// [14, 99, 85]
+console.log(differenceInAges([82, 15, 6, 38, 35])); //[6,82,76]
+console.log(differenceInAges([57, 99, 14, 32])); // [14, 99, 85]
