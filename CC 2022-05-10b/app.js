b558f4e3e204becabe9ecb4c56b8109aad57db67
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-05-10 16:21:49 -0400

    Add coding challenge for 05-10b-2022

--- CC 2022-05-10b/app.js
+++ CC 2022-05-10b/app.js
@@ -3,3 +3,12 @@
 E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
 
 Note: You can expect all of the inputs to be the same length. */
+
+function tripleTrouble(one, two, three){
+  
+    let str= '';
+    for(let i = 0;i < one.length;i++){
+      str+= one[i] + two[i] + three[i]
+    }
+    return str;
+   }
