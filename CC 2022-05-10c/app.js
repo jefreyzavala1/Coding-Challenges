a9358d720ec708aefdcdf637a84aa95592c8a286
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-05-10 16:34:39 -0400

    Add coding challenge for 05-10c-2022

--- CC 2022-05-10c/app.js
+++ CC 2022-05-10c/app.js
@@ -18,6 +18,6 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
   return remainingBlue / (remainingBlue + remainingRed);
 }
 
-console.log(guessBlue(5, 5, 2, 3));//0.6
-console.log(guessBlue(5, 7, 4, 3));// 0.2
-console.log(guessBlue(12, 18, 4, 6));// 0.4
+console.log(guessBlue(5, 5, 2, 3)); //0.6
+console.log(guessBlue(5, 7, 4, 3)); // 0.2
+console.log(guessBlue(12, 18, 4, 6)); // 0.4
