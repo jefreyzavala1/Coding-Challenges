1fc58ed9930d78185f6e2ddcf7f13b345d6a1dfe
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-05-12 11:54:30 -0400

    Add coding challenge for 05-12-2022

--- CC 2022-05-12/app.js
+++ CC 2022-05-12/app.js
@@ -19,3 +19,8 @@ var quote = function (fighter) {
     return `I'd like to take this chance to apologize.. To absolutely NOBODY!`;
   }
 };
+
+console.log(quote("george saint pierre")); // "I am not impressed by your performance."
+console.log(quote("conor mcgregor")); // "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+console.log(quote("George Saint Pierre")); // "I am not impressed by your performance."
+console.log(quote("Conor McGregor")); // "I'd like to take this chance to apologize.. To absolutely NOBODY!"
