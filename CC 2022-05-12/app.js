4562031c58a5774854133e7322170aede8b63ddf
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-05-12 11:52:41 -0400

    Add coding challenge for 05-12-2022

--- CC 2022-05-12/app.js
+++ CC 2022-05-12/app.js
@@ -11,3 +11,11 @@ Good Luck!
 
 Note
 The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive) */
+
+var quote = function (fighter) {
+  if (fighter.toLowerCase() === "george saint pierre") {
+    return `I am not impressed by your performance.`;
+  } else if (fighter.toLowerCase() === "conor mcgregor") {
+    return `I'd like to take this chance to apologize.. To absolutely NOBODY!`;
+  }
+};
