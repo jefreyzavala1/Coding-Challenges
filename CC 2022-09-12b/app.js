efe55b4bdc385b234a9b2355bb2c3672eaa4b9bf
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-09-13 11:37:21 -0400

    Add coding challenge for 09-13-2022

--- CC 2022-09-12b/app.js
+++ CC 2022-09-12b/app.js
@@ -11,27 +11,13 @@
 // 1 -->  1
 // 2 --> 3 + 5 = 8
 
-
 function rowSumOddNumbers(n) {
-	
-    //p: an integer representing the index of the odd triangle
-    //r:the sum of all odd integers at a given index
-    //e:n = 1 returns 1, 42 returns 74088
-    //p:lets have an array of multiple arrays where each index would represent an array of n level
-    //will need a for loop where the length of the outer array is of length n -1
-    let countOdd = -1;
-    let array = [];
-    let innerArray = [];
-    for(let i = 0;i <= n-1;i++){
-      for(let j = 1;j <=n;j++){
-      innerArray.push(countOdd + 2)
-        console.log(innerArray)
-        
-      }
-      //[1,3]
-     
-      array.push(innerArray);
-      
-    }
-    //console.log(array);
-  }
+  //p: an integer representing the index of the odd triangle
+  //r:the sum of all odd integers at a given index
+  //e:n = 1 returns 1, 42 returns 74088
+  //p:lets have an array of multiple arrays where each index would represent an array of n level
+  //will need a for loop where the length of the outer array is of length n -1
+  return Math.pow(n, 3);
+}
+
+console.log(rowSumOddNumbers(42)); //74088
