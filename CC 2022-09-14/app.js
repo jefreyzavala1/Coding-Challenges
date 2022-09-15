eb181f9d71df898381096beae088644d7a3e21db
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-09-14 20:02:57 -0400

    Add coding challenge for 09-14-2022

--- CC 2022-09-14/app.js
+++ CC 2022-09-14/app.js
@@ -1,4 +1,3 @@
-
 // Given a list of integers, determine whether the sum of its elements is odd or even.
 
 // Give your answer as a string matching "odd" or "even".
@@ -16,21 +15,19 @@
 // Output: "even"
 
 function oddOrEven(array) {
-   
-    //p:an array of intergers
-    //r:return if sum of array is even or odd
-    //e: [0] => "even", [0,1,4]=>odd, [0.-1,-5]
-    //p:use reduce method to calculate sum of array then mod result with %2 if equals 0 then even else odd
-  
-  let sum = array.reduce((accu,current)=> accu + current,0);
-  if(sum % 2 === 0){
-    return 'even'
-  }else{
-    return 'odd'
+  //p:an array of intergers
+  //r:return if sum of array is even or odd
+  //e: [0] => "even", [0,1,4]=>odd, [0.-1,-5]
+  //p:use reduce method to calculate sum of array then mod result with %2 if equals 0 then even else odd
+
+  let sum = array.reduce((accu, current) => accu + current, 0);
+  if (sum % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
   }
-  
 }
 
-console.log(oddOrEven([0,1,5]));//'even'
-console.log(oddOrEven([0,1,3]));//'even'
-console.log(oddOrEven([1]));//'odd'
+console.log(oddOrEven([0, 1, 5])); //'even'
+console.log(oddOrEven([0, 1, 3])); //'even'
+console.log(oddOrEven([1])); //'odd'
