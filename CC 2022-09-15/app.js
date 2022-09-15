9d9a247d551ac760b6afc386b81d2fc14c2092be
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-09-14 23:59:03 -0400

    Add coding challenge for 09-15-2022

---
+++ CC 2022-09-15/app.js
@@ -0,0 +1,24 @@
+var number=function(array){
+
+    //p: an array of strings , empty array can be passed
+   //r: an array where each string is mapped with correct order starting with one
+  //e: [] => [], ['a','b','c']=> ['1: a', '2: b' , '3: c']
+   //p: loop array have a counter that starts at 1 then append : ' ' arr[i] push to new array or also use map
+    
+  //   let string = ': ';
+  //   let newArray = [];
+  //   let counter = 1;
+  //   if(array.length===0){
+  //     return [];
+  //   }
+  //   array.forEach(value=>{
+  //     let str = counter + string + value;
+  //     newArray.push(str);
+  //     counter++;
+  //   })
+  //   return newArray;
+    
+    return array.map((element,index)=> ++index +': ' + element)
+    
+    
+  }
