2918c132d7ed68949b93aa5829ac2032abfe4365
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-10-18 17:41:04 -0400

    Add coding challenge for 10-18-2022

--- CC 2022-10-18/app.js
+++ CC 2022-10-18/app.js
@@ -1,14 +1,13 @@
-function sumMix(x){
+function sumMix(x) {
+  //array of numbers which can either represented as numbers or strings
+  //return a number which describes the sum of the whole array
+  //[9,3 '7','3']=> 22
+  //['5',5]=>10
+  //use reduce and Number to convert all to number and sum all and return
 
-    //array of numbers which can either represented as numbers or strings
-    //return a number which describes the sum of the whole array
-    //[9,3 '7','3']=> 22
-    //['5',5]=>10
-    //use reduce and Number to convert all to number and sum all and return
-    
-    return x.reduce((accu,current)=> accu +Number(current),0 )
-  }
+  return x.reduce((accu, current) => accu + Number(current), 0);
+}
 
-  console.log((sumMix([9, 3, '7', '3']), 22));
-    console.log((sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42)); 
-    console.log((sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41)); 
+console.log((sumMix([9, 3, "7", "3"]), 22));
+console.log((sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7]), 42));
+console.log((sumMix(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"]), 41));
