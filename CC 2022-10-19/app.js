5c08cddf0fc7c68df2feff3126bd5f4214da93b3
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-10-19 18:07:54 -0400

    Add coding challenge for 10-19-2022

--- CC 2022-10-19/app.js
+++ CC 2022-10-19/app.js
@@ -6,7 +6,16 @@
 // getAverage([1,2,3,4,5,]),3);
 // getAverage([1,1,1,1,1,1,1,2]),1)
 
-function getAverage(arr) {}
+function getAverage(arr) {
+    //loop thorough array to get sum of all values
+    let sum = arr.reduce((accu,current)=> accu + current,0)
+    //avg-> sum/arr.length
+
+    //return
+    return Math.floor(sum / arr.length)
+
+
+}
 
 console.log(getAverage([2, 2, 2, 2]), 2);
 console.log(getAverage([1, 2, 3, 4, 5]), 3);
