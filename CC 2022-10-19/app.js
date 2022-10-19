9ea575e1cfabdb0fbecda289eae07b31309dde7d
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-10-19 18:04:26 -0400

    Add coding challenge for 10-19-2022

--- CC 2022-10-19/app.js
+++ CC 2022-10-19/app.js
@@ -6,9 +6,7 @@
 // getAverage([1,2,3,4,5,]),3);
 // getAverage([1,1,1,1,1,1,1,2]),1)
 
-function getAverage(arr){
+function getAverage(arr) {}
 
-}
-
-console.log(getAverage([2,2,2,2]),2);
-console.log(getAverage([1,2,3,4,5,]),3);
+console.log(getAverage([2, 2, 2, 2]), 2);
+console.log(getAverage([1, 2, 3, 4, 5]), 3);
