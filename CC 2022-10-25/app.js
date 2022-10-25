29000ad13ade5985f972d60ac8224455dff1395f
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-10-25 17:17:04 -0400

    Add coding challenge for 10-25-2022

--- CC 2022-10-25/app.js
+++ CC 2022-10-25/app.js
@@ -8,3 +8,13 @@
 // solve([[1,2],[4,4],[5,6,6]]),4)
 // solve([[1,2],[3,4],[5,6]]),8)
 // solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
+//You have an array full of arrays. Map through the parent array and make sure each sub array is a set aka no duplicate numbers.
+// Once each sub array contains no duplicates you can just reduce multiplying the length of each sub array together
+//to get the total combinations
+
+function solve(arr) {
+  let newArray = arr.map((item) => [...new Set(item)]);
+  return newArray.reduce((accu, current) => accu * current.length, 1);
+}
+
+console.log(4, solve([[1, 2], [4], [5, 6]]));
