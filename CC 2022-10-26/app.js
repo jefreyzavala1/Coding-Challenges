03fe84e73bd7684420cab2bd30ad5e01e6f5bb80
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-10-26 17:16:37 -0400

    Add coding challenge for 10-26-2022

--- CC 2022-10-26/app.js
+++ CC 2022-10-26/app.js
@@ -7,15 +7,15 @@
 
 // Repetition in array/list's numbers could occur , so don't Remove Duplications .
 
-// nthSmallest({3,1,2} ,2) ==> return (2) 
-// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
-// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
-// nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92) 
+// nthSmallest({3,1,2} ,2) ==> return (2)
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7)
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7)
+// nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92)
 
-function nthSmallest(array,value){
-    return array.sort((a,b)=> a - b)[value-1]
+function nthSmallest(array, value) {
+  return array.sort((a, b) => a - b)[value - 1];
 }
 
 //const nthSmallest = (arr,pos)=> array.sort((a,b)=> b -a)[pos-1]
-console.log(nthSmallest([3,1,2],2));//2
-console.log(nthSmallest([177,225,243,-169,-12,-5,2,92],5));//92
+console.log(nthSmallest([3, 1, 2], 2)); //2
+console.log(nthSmallest([177, 225, 243, -169, -12, -5, 2, 92], 5)); //92
