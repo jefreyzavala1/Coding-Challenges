fe944a07e886517d8c26b77236b73cb55cafca77
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-10-27 17:19:13 -0400

    Add coding challenge for 10-27-2022

--- CC 2022-10-27/app.js
+++ CC 2022-10-27/app.js
@@ -6,4 +6,20 @@
 // "din"      =>  "((("
 // "recede"   =>  "()()()"
 // "Success"  =>  ")())())"
-// "(( @"     =>  "))((" 
+// "(( @"     =>  "))(("
+
+function duplicateEncode(word) {
+  //input a word as a string can containg english letters as well as especial chars
+  // i would like to return a new string in which each char represents whether char from orignal
+  // string repeats or not .if indeed does repeat place '(' if not ')'
+
+  //turn words into an array for looping
+  // use map by passing the value at each iteration of the charArray
+  //use indexof and last index of to determine if char is the only one within the string or not.
+
+  const charArray = word.split("").map((element) => element.toLowerCase());
+  let newArray = charArray.map((element) =>
+    charArray.indexOf(element) === charArray.lastIndexOf(element) ? "(" : ")"
+  );
+  return newArray.join("");
+}
