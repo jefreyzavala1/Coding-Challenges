c16df339a9d57ce5eb5a47f01ae44174079b9c53
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-11-09 17:59:27 -0500

    Add coding challenge for 11-09-2022

--- CC 2022-11-09/app.js
+++ CC 2022-11-09/app.js
@@ -12,3 +12,33 @@
 // arrayDiff([3,4], [3]), [4], "a was [3,4], b was [3]"
 
 // arrayDiff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []"
+function arrayDiff(a, b) {
+  
+    let obj = {};
+      //"1":[1]
+      //"2":[2,2,2,2]
+      let array = [];
+      for(let i = 0; i < a.length;i++){
+        if(obj[a[i]]){
+        obj[a[i]].push(a[i])
+      }else{
+        obj[a[i]] = [a[i]]
+      }
+      }
+    console.log(obj)
+      //look through b and remove such property from a
+      for (let i = 0; i < b.length;i++){
+        if(obj[b[i]]){
+          delete obj[b[i]]
+        }
+      }
+    //  let numbers = Object.values(obj);
+    
+    // console.log(numbers); // [4,93,10]
+    //   return numbers
+      for(const property in obj){
+        array.push(...obj[property])
+      }
+      return array
+    }
+    
