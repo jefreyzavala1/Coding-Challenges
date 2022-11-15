c76d6e75f65008152252404a3eacae034d9fef4f
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-11-15 16:52:04 -0500

    Add coding challenge for 11-15-2022

--- CC 2022-11-15/app.js
+++ CC 2022-11-15/app.js
@@ -1,13 +1,9 @@
-// You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
-// The input to the function will be an array of three distinct numbers.
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
-// For example:
-
-// gimme([2, 3, 1]) => 0
-// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
-
-// Another example (just to make sure it is clear):
-
-// gimme([5, 10, 14]) => 1
-// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+// sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+// sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
