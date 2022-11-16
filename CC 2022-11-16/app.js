78db0212b3d6fa4f6e441d27663e2a98d04eb1fc
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-11-16 17:18:13 -0500

    Add coding challenge for 11-16-2022

--- CC 2022-11-16/app.js
+++ CC 2022-11-16/app.js
@@ -8,16 +8,20 @@
 // If the average length is not an integer, use Math.round().
 // The input array's length > 1
 
-function averageLength(array) { 
-    // good luck
-    //input an array of strings where each string is the same type of letter
-    //return an array in which the length of each element is equal to the average of the elements of the previous array?
-    //['u','y']=>['u','y']
-    //if decimal ceil function 
-    //use map to extract the length of string and reduce to ceil(sum/array.length) use map again and repeat?
-    // return that new array
-    let average = Math.round(array.map(element=> element.length).reduce((accu,current)=> accu + current,0)/array.length)
-    //3
-    
-    return array.map(letter=> letter[0].repeat(average))
-  }
+function averageLength(array) {
+  // good luck
+  //input an array of strings where each string is the same type of letter
+  //return an array in which the length of each element is equal to the average of the elements of the previous array?
+  //['u','y']=>['u','y']
+  //if decimal ceil function
+  //use map to extract the length of string and reduce to ceil(sum/array.length) use map again and repeat?
+  // return that new array
+  let average = Math.round(
+    array
+      .map((element) => element.length)
+      .reduce((accu, current) => accu + current, 0) / array.length
+  );
+  //3
+
+  return array.map((letter) => letter[0].repeat(average));
+}
