edcc78f767ff5225ab084d058bb006207a7b5fcb
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-11-21 17:33:10 -0500

    Add coding challenge for 11-21-2022

--- CC 2022-11-21/app.js
+++ CC 2022-11-21/app.js
@@ -3,3 +3,33 @@
 // "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
 
 // --> "alpha beta gamma delta alpha beta gamma delta"
+const removeConsecutiveDuplicates = (s) => {
+  //input a string of words
+  // return a string that consist of the initial string but with containting of the first word of word is
+  // repeated consecutive
+  //"alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"->
+  // "alpha beta gamma delta alpha beta gamma delta"
+
+  //iterate through string one by one and for each word that is consecutive splice string to a certain point and keep going
+
+  let arrayWords = s.split(" ");
+  let newArray = [];
+  while (arrayWords.length != 0) {
+    //holding index
+    //hold first word
+    let i = 0;
+    let first = arrayWords[0];
+    if (first == arrayWords[1]) {
+      i = 2;
+      while (first === arrayWords[i]) {
+        i++;
+      }
+      //2
+      newArray.push(first);
+      arrayWords.splice(arrayWords[0], i);
+    } else {
+      newArray.push(arrayWords.shift());
+    }
+  }
+  return newArray.join(" ");
+};
