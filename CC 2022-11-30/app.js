3a442834d176c8fc789e0eb91a1039c52be7dfec
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-11-30 17:23:43 -0500

    Add coding challenge for 11-30-2022

--- CC 2022-11-30/app.js
+++ CC 2022-11-30/app.js
@@ -10,3 +10,30 @@
 // Examples
 // "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
 // "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
+function absentVowel(x) {
+  //a string of words
+  //return the index in which a vowel does not appear a,e,i,o,u
+  //dont need to worry about capitals each string will contain all the vowels except for one
+
+  //obj containing vowels matching value from 0-4
+  //{
+  // 'a':0, 'e':1, 'i':2,'o':3,'u':4
+  //  }
+
+  //split->loop obj-> validate if obj property found in array using includes if false return the value of that property
+  // which would be the index
+  let obj = {
+    a: 0,
+    e: 1,
+    i: 2,
+    o: 3,
+    u: 4,
+  };
+
+  let charArray = x.split("");
+  for (const property in obj) {
+    if (!charArray.includes(property)) {
+      return obj[property];
+    }
+  }
+}
