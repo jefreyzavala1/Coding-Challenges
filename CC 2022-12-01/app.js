717506884fb904b707a5a72cb216ee4ffc46cfcb
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-12-01 17:09:18 -0500

    Add coding challenge for 12-01-2022

--- CC 2022-12-01/app.js
+++ CC 2022-12-01/app.js
@@ -1,1 +1,19 @@
 // Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
+
+//input is a number
+//loop all the way to that number if number is divisible by 2 print "Fizz"
+//if divisible by 3 print "Buzz" but when its divisible by both print "Fizz Buzz"
+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if ((i % 2 === (0 % i) % 3) === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 2 === 0) {
+      console.log("Fizz");
+    } else if (i % 3) {
+      console.log("Buzz");
+    }
+  }
+}
+
+fizzBuzz(15);
