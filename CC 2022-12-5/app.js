de72e6c6c4a2d4cd1eabf0f93c206663ba0c137e
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-12-05 18:06:05 -0500

    Add coding challenge for 12-05-2022

---
+++ CC 2022-12-5/app.js
@@ -0,0 +1,20 @@
+// Count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+//input string of characters string can empty if so return {}
+//return an object with key value pairs such that keys being char pairs to how many instances of that char
+//occur in the string
+
+function occuringChar(s) {
+  if (s.length === 0) {
+    return {};
+  }
+
+  return s
+    .split("")
+    .reduce((obj, item) => (obj[item] ? obj[item]++ : (obj[item] = 1)), {});
+}
+
+console.log(occuringChar("aba"), "{'a':2,'b':1}");
+console.log(occuringChar("dog"), "{'d':1,'o':1,'g':1}");
