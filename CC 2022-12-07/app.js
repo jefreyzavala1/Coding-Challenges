eea95a11e2b2b04505e00427788e884dff73222c
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-12-08 01:04:13 -0500

    Add coding challenge for 12-07-2022

--- CC 2022-12-07/app.js
+++ CC 2022-12-07/app.js
@@ -7,3 +7,29 @@
 // });
 
 // No String or Array Methods (well brute force it first, but then no methods)!
+//input a string characters
+//return the character that basically appears the most in string
+//'aacd'=> 'a'
+//'hello world!'=>l
+
+function maxCharacter(s) {
+  let obj = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]]) {
+      obj[s[i]]++;
+    } else {
+      obj[s[i]] = 1;
+    }
+  }
+
+  let maxChar = "";
+  let currentValue = 0;
+  for (const property in obj) {
+    if (obj[property] > currentValue) {
+      maxChar = property;
+      currentValue = obj[property];
+    }
+  }
+  return maxChar;
+}
