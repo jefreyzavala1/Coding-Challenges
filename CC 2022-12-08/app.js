c330ef87357727502ae89ffa244dd14dbbcf80f2
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-12-08 01:45:16 -0500

    Add coding challenge for 12-08-2022

--- CC 2022-12-08/app.js
+++ CC 2022-12-08/app.js
@@ -3,3 +3,47 @@
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
 // Return the head of the merged linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  if (list2 === null) {
+    return list1;
+  }
+  if (list1 === null) {
+    return list2;
+  }
+
+  const head = new ListNode();
+  console.log(head);
+  let current = head;
+  while (list1 != null && list2 != null) {
+    if (list1.val <= list2.val) {
+      current.next = new ListNode(list1.val);
+      list1 = list1.next;
+    } else {
+      current.next = new ListNode(list2.val);
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1) {
+    current.next = list1;
+  }
+  if (list2) {
+    current.next = list2;
+  }
+
+  return head.next;
+};
