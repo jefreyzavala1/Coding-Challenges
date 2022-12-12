a96062bf8ccb28713e959a5ab98b27bdd04a7256
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-12-12 17:28:56 -0500

    Add coding challenge for 12-12-2022

--- CC 2022-12-12/app.js
+++ CC 2022-12-12/app.js
@@ -12,3 +12,14 @@
 
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
+
+var containsDuplicate = function (nums) {
+  //input an array of nums
+  //return if every number in array is distinct meaning no dups
+  //create a hashmap and loop through array if property found in object
+  //then return false and make it return true by default which gives an indication that whole array ran and no
+  //dups were found
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]), "true");
+console.log(containsDuplicate([1, 2, 3, 4]), "false");
