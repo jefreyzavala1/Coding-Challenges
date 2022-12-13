fc154e115c91af5e49a2cfa3487525b339d1a3d2
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-12-13 17:03:29 -0500

    Add coding challenge for 02-14-2022

--- CC 2022-12-13/app.js
+++ CC 2022-12-13/app.js
@@ -10,3 +10,31 @@
 
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
+
+var majorityElement = function (nums) {
+  //input an array of nums
+  //output the element found the most in array
+  //use a hashmap and store each number as property and hold a value such that it represents its
+  //occurence in the nums array
+  //loop through object and extract the num whole value is the biggest and return it
+
+  let obj = {};
+
+  for (const num of nums) {
+    if (obj[num] !== undefined) {
+      obj[num]++;
+    } else {
+      obj[num] = 1;
+    }
+  }
+
+  let biggest = null;
+  let num = null;
+  for (const property in obj) {
+    if (obj[property] > biggest) {
+      biggest = obj[property];
+      num = Number(property);
+    }
+  }
+  return num;
+};
