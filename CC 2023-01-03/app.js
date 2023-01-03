88464b8e99189c202e0c00ce1d07ccc593dd6907
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-01-03 18:12:59 -0500

    Add coding challenge for 01-03-2023

--- CC 2023-01-03/app.js
+++ CC 2023-01-03/app.js
@@ -19,3 +19,11 @@
 // Input: n = 4
 // Output: 3
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+function fib(n) {
+  let arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
