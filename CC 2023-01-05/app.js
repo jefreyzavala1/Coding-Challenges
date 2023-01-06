c0171181920776361cece5f7ccb723fc385b87a3
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-01-06 16:17:02 -0500

    Add coding challenge for 01-06-2023

--- CC 2023-01-05/app.js
+++ CC 2023-01-05/app.js
@@ -11,3 +11,22 @@
 // Input: costs = [1,3,2,4,1], coins = 7
 // Output: 4
 // Explanation: The boy can buy ice cream bars at indices 0,1,2,4 for a total price of 1 + 3 + 2 + 1 = 7.
+
+var maxIceCream = function (costs, coins) {
+  //input a cost array which contains the prices of each chocolate bar
+  //coins int value which describes the amount of money the kid has
+
+  let numBars = 0;
+
+  let sortCosts = costs.sort((a, b) => a - b);
+  let currentCoins = coins;
+  for (const bar of costs) {
+    if (currentCoins >= bar) {
+      numBars++;
+      currentCoins = currentCoins - bar;
+    } else {
+      break;
+    }
+  }
+  return numBars;
+};
