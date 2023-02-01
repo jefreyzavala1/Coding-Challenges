219147ce2acf713d5039602d5b48002b8b6283b1
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-02-01 18:09:59 -0500

    added coding challange

--- CC 2023-02-01/app.js
+++ CC 2023-02-01/app.js
@@ -1,3 +1,10 @@
 // Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
 
 // The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+function mygcd(a, b) {
+  if (b === 0) {
+    return 0;
+  }
+  return mygcd(b, a % b);
+}
