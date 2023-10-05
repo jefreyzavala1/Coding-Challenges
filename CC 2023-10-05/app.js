0c89eaa41851badb4fd785db2307ce30d918d2bb
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-10-05 17:33:39 -0400

    coding challenge

---
+++ CC 2023-10-05/app.js
@@ -0,0 +1,7 @@
+var hasCycle = function(head){
+    //p:head of the lists
+    //r:return true or false if a cycle exists
+    //e:pos its the index of the node that tail points to.
+    //p:use a set to store visited nodes if we encounter the same visited node
+    //it means there is a cycle.
+}
