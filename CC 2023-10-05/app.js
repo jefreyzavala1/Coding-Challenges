61b8e420a9adcd4f56abeb31d396a5205199d188
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-10-05 17:35:25 -0400

    coding challenge

--- CC 2023-10-05/app.js
+++ CC 2023-10-05/app.js
@@ -4,4 +4,9 @@ var hasCycle = function(head){
     //e:pos its the index of the node that tail points to.
     //p:use a set to store visited nodes if we encounter the same visited node
     //it means there is a cycle.
+    if(!head){
+        return false
+    }
+    let current = head;
+    
 }
