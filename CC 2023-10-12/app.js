b0762e75921377be8b40a660144838b6eb853ec6
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-10-12 21:06:16 -0400

    coding challege 10-12-2023

---
+++ CC 2023-10-12/app.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var maxSubArray = function (nums) {
+  //p:nums an array of numbers can be negative numbers
+  //r:return the largest sum
+  //e:[-2,1,-3,4,-1,2,-5,4]=> [4,-1,2,1]=>6
+  //p:using kadane algorithm
+};
