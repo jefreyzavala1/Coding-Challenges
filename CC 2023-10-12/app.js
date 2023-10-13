d8ce7f0b836b6a3f05f950435e6c67febfd2359e
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-10-12 21:07:57 -0400

    coding challege 10-12-2023

--- CC 2023-10-12/app.js
+++ CC 2023-10-12/app.js
@@ -8,4 +8,14 @@ var maxSubArray = function (nums) {
   //r:return the largest sum
   //e:[-2,1,-3,4,-1,2,-5,4]=> [4,-1,2,1]=>6
   //p:using kadane algorithm
+  let max_current = nums[0];
+  let max_global = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    max_current = Math.math(max_current, max_current + nums[i]);
+    if (max_current > max_global) {
+      max_global = max_current;
+    }
+  }
+  return max_global;
 };
